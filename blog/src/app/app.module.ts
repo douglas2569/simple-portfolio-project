e7ddae683d84d449502adeb3f9387d8635f9d072
douglas <carlosdouglas@alu.ufc.br>
2024-01-11 09:12:14 -0300

    created folder structure

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoverComponent } from './components/cover/cover.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ProjectComponent } from './components/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoverComponent,
+    FooterComponent,
+    AboutComponent,
+    NavigationMenuComponent,
+    SkillComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
