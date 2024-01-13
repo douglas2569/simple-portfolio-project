a93752e40f8a6ec46a8375962bae9f3448b4a76a
douglas2569 <carlosdouglas@alu.ufc.br>
2024-01-13 16:07:36 -0300

    migrate about and socialmedia

---
+++ dashboard-api/database/migrations/2024_01_13_185149_create_abouts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover_photo');
+            $table->string('profile_photo');
+            $table->string('name');
+            $table->string('position');
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('social_media_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
