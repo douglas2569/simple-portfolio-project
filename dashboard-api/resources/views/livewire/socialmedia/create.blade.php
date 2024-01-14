118547aac347c5dc61d12aec0a41a9a45560f09c
douglas2569 <carlosdouglas@alu.ufc.br>
2024-01-13 21:08:04 -0300

    update dashboard component social media

--- dashboard-api/resources/views/livewire/socialmedia/create.blade.php
+++ dashboard-api/resources/views/livewire/socialmedia/create.blade.php
@@ -29,7 +29,8 @@ new class extends Component {
             class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
         />
 
-        <x-input-error :messages="$errors->get('message')" class="mt-2" />
+        <x-input-error :messages="$errors->get('icon')" class="mt-2" />
+        <x-input-error :messages="$errors->get('name')" class="mt-2" />
         <x-primary-button class="mt-4">{{ __('Create') }}</x-primary-button>
 
     </form>
