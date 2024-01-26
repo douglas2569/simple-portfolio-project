c0d811829754b35a9d341b33c42f3913327baca4
douglas <carlosdouglas@alu.ufc.br>
2024-01-26 11:02:27 -0300

    create view_projects_technologies

---
+++ dashboard-api/database/migrations/2024_01_26_135633_create_projects_technologies_view.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement($this->createView());
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement($this->dropView());
+    }
+
+    private function createView():string
+    {
+        return "
+            CREATE VIEW view_projects_technologies
+            AS
+            SELECT technologies.id, technologies.id AS technology_id, technologies.name AS technology_name, technologies.color AS technology_color, technologies.id AS technology_id, technologies.created_at AS technology_created_at, technologies.updated_at AS technology_updated_at,
+            project.id AS project_id, projects.thumbnail AS project_thumbnail, projects.name AS project_name, projects.created_at AS project_created_at, projects.updated_at AS project_updated_at, project.video_youtube_id AS project_video_youtube_id, project.description AS project_description
+            FROM project_technology
+            INNER JOIN projects
+            ON projects.id = project_technology.project_id
+            INNER JOIN technologies
+            ON technologies.id = project_technology.technology_id";
+    }
+
+    private function dropView():string
+    {
+        return "DROP VIEW IF EXISTS 'view_projects_technologies';";
+    }
+
+
+};
