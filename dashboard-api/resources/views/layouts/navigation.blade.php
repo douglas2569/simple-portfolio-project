c5107f9398657d8d9bcafb48e8e4e94c966a4dfb
douglas2569 <carlosdouglas@alu.ufc.br>
2024-01-28 07:26:54 -0300

    update dashboard about

--- dashboard-api/resources/views/layouts/navigation.blade.php
+++ dashboard-api/resources/views/layouts/navigation.blade.php
@@ -54,7 +54,6 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
@@ -115,10 +114,10 @@
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                    <x-responsive-nav-link
+                    :href="route('logout')"
+                     onclick="event.preventDefault();
+                    this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
