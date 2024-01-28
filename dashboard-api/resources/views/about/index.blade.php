def11ff8d6880e062c0c60448b291514ce592009
douglas2569 <carlosdouglas@alu.ufc.br>
2024-01-28 07:26:23 -0300

    update about dashboard

--- dashboard-api/resources/views/about/index.blade.php
+++ dashboard-api/resources/views/about/index.blade.php
@@ -34,11 +34,14 @@
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
             />
 
-            <div>
-                @csrf
-                <textarea name="description" id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your description here...">
-                </textarea>
-            </div>
+
+            @csrf
+            <textarea
+                name="description"
+                placeholder="{{ __('Write your description here...') }}"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            ></textarea>
 
             <x-input-error :messages="$errors->get('profilePhoto')" class="mt-2" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
