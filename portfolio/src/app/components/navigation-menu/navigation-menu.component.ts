45edd98985a46ad4386f44e275af134d48638bd5
douglas <carlosdouglas@alu.ufc.br>
2024-01-31 10:51:03 -0300

    update navigation component

--- portfolio/src/app/components/navigation-menu/navigation-menu.component.ts
+++ portfolio/src/app/components/navigation-menu/navigation-menu.component.ts
@@ -6,13 +6,35 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./navigation-menu.component.css']
 })
 export class NavigationMenuComponent implements OnInit {
+  statusDrawer:boolean = true
 
   constructor() { }
 
   ngOnInit(): void {
-   
+
   }
 
+  toggleDrawer(){
+    const drawer = document.querySelector('#drawer')
+    const body = document.querySelector('body')
+
+    drawer?.classList.toggle('transform-none')
+    body?.classList.toggle('overflow-hidden')
+
+    drawer?.setAttribute('aria-modal',`${this.statusDrawer}`)
+
+    if(this.statusDrawer){
+      drawer?.setAttribute('role',`dialog`)
+      document.querySelector('.z-30')?.classList.remove('hidden')
+    }else{
+      document.querySelector('.z-30')?.classList.add('hidden')
+      drawer?.removeAttribute('role')
+    }
+
+    this.statusDrawer = !this.statusDrawer
+
+
+  }
 
 
 }
--- portfolio/src/app/pages/project-content/project-content.component.ts
+++ portfolio/src/app/pages/project-content/project-content.component.ts
@@ -41,4 +41,8 @@ export class ProjectContentComponent implements OnInit {
     }
   }
 
+  back(){
+    history.back()
+  }
+
 }
