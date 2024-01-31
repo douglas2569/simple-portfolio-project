8dc6ad441cb442e5e6d9ce8b2c358e277dd71c9c
douglas <carlosdouglas@alu.ufc.br>
2024-01-31 17:37:15 -0300

    update project component and global styles

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,6 +44,7 @@ import { UrlImagePipe } from './pipes/url-image.pipe';
     YouTubePlayerModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
