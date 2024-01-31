394433e2a9a8d6b4b3c544cbdb9077a5cb6ea827
douglas <carlosdouglas@alu.ufc.br>
2024-01-31 20:07:21 -0300

    update login

--- dashboard-api/resources/views/auth/login.blade.php
+++ dashboard-api/resources/views/auth/login.blade.php
@@ -33,11 +33,14 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+            <a href="{{ route('register') }}" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
