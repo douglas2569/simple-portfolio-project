de0a1e04d528c61588b6eec0063caba3c6840be4
douglas <carlosdouglas@alu.ufc.br>
2024-01-31 19:02:05 -0300

    valdition contact component

--- portfolio/src/app/components/contact/contact.component.ts
+++ portfolio/src/app/components/contact/contact.component.ts
@@ -1,21 +1,38 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {  
+export class ContactComponent implements OnInit {
   @Input()
   to!:string
-  @Input()
-  message:string = "Olá Douglas, "
+
+  emailForm!:FormGroup
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.emailForm = new FormGroup({
+      subject: new FormControl('',Validators.required),
+      message: new FormControl('',Validators.required),
+    })
+  }
 
-  sendEmail():void{    
+
+  get subject(){
+    return this.emailForm.get('subject')!
+  }
+
+  get message(){
+    return this.emailForm.get('message')!
+  }
+
+  submit():void{
+    if(this.emailForm.invalid) return
+
     console.log(`${this.message} | ${this.to} `)
   }
 
