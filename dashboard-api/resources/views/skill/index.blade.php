0d1a15a0170c76a22c98c04a9771f090c9a123e9
douglas <carlosdouglas@alu.ufc.br>
2024-02-02 11:39:18 -0300

    update skill dashboard

--- dashboard-api/resources/views/skill/index.blade.php
+++ dashboard-api/resources/views/skill/index.blade.php
@@ -40,7 +40,7 @@
                                         type="checkbox"
                                         value="{{$technology->id}}"
                                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                    <label for="{{$technology->name}}-checkbox" class="w-full py-3 ms-2 text-base font-medium text-gray-900 dark:text-gray-300">{{$technology->name}}</label>
+                                    <label for="{{$technology->name}}-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{$technology->name}}</label>
                                 </div>
                             </li>
                         @endforeach
@@ -109,7 +109,7 @@
                             <img class="w-20" src="{{ $icon }}" alt="{{ $skill->name }}" srcset="">
                             <p class="mt-4 text-base">{{ $skill->name }}</p>
                         </div>
-                        <ul class="grid mt-4 text-gray-900 justify-center text-base sm:grid-cols-5">
+                        <ul class="grid mt-4 text-gray-900 justify-center text-sm sm:grid-cols-5">
                             @foreach($myTechnologies as $technology)
                                 <li>{{$technology->technology_name}}</li>
                             @endforeach
